e848e7e5d0cb2e2821a71230ee35ed541636a692
durveshvp <168174162+durveshvp@users.noreply.github.com>
2025-05-20 11:06:47 +0530

    added pysqlite3 import in app.py file

--- ONCO-ARISTO-BOT/app.py
+++ ONCO-ARISTO-BOT/app.py
@@ -1,4 +1,12 @@
 import os
+import sys
+
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import streamlit as st
 import tempfile
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredWordDocumentLoader
